39574c3bbdf26f1ec52bec0c2ce9bb6b742d39cd
Megan Cho <ymegan08@gmail.com>
2020-05-01 20:53:02 -0700

    Updates

---
+++ precache-manifest.46daea91279e8ac27841142a8cfad37d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ea7310c43b7e18b26264fc2fa515c1",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "192b0c440986d3966419",
+    "url": "/clicky-game/static/css/main.3dfb16e7.chunk.css"
+  },
+  {
+    "revision": "7b75650bb606259aaba2",
+    "url": "/clicky-game/static/js/2.c6710c3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.c6710c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192b0c440986d3966419",
+    "url": "/clicky-game/static/js/main.91bac516.chunk.js"
+  },
+  {
+    "revision": "b1a02565af27a65f66a4",
+    "url": "/clicky-game/static/js/runtime-main.552a697a.js"
+  }
+]);
